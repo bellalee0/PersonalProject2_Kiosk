f43b24773c0ac3551d4bf31b3e5675de39830af0
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-22 16:18:03 +0900

    Lv 2 내용 파일 분리

---
+++ src/lv2/MenuItem.java
@@ -0,0 +1,20 @@
+package lv2;
+
+public class MenuItem {
+    // 속성
+    String name;
+    double price;
+    String description;
+
+    // 생성자
+    MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // 기능
+    public String toString() {
+        return name + " | W " + price + " | " + description;
+    }
+}
