c158598e078d7652428d2fd7175168dad61143f6
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-22 19:58:12 +0900

    Lv 4: Menu 클래스 구현
    1. menuItems 컬렉션 생성
    2. 생성자
      - Menu() : 이름 없이 Menu 객체 생성
      - Menu(String categoryName) : 카테고리 이름을 넣어서 Menu 객체 생성
    3. toString() 오버라이드 : categoryName 출력
    4. saveMenus(List<MenuItem> menus) : MenuItem 객체들을 meniItems 컬렉션에 저장
    5. printMenus() : 리스트에 있는 MenuItem을 순차적으로 프린트

--- src/lv4/Menu.java
+++ src/lv4/Menu.java
@@ -1,4 +1,30 @@
 package lv4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
+    // 속성
+    List<MenuItem> menuItems = new ArrayList<>();
+    String categoryName;
+
+    // 생성자(오버라이드)
+    Menu() {}
+    Menu(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    // toString 오버라이드
+    @Override
+    public String toString() { return categoryName; }
+
+    // MenuItem 객체를 리스트에 저작
+    void saveMenus(List<MenuItem> menus) { this.menuItems = new ArrayList<>(menus); }
+
+    // 리스트에 있는 MenuItem을 순차적으로 프린트
+    void printMenus() {
+        for (int i = 1; i <= menuItems.size(); i++) {
+            System.out.println(i + ". " + menuItems.get(i - 1));
+        }
+    }
 }
