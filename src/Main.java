16ed7fdbe6f9625179ad6866c364400d02a6b4b9
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-22 12:55:09 +0900

    Lv 2: main 함수에 MenuItem 클래스 활용
    1. menuItems 리스트 컬렉션 생성
    2. menuItems에 MenuItem 객체 생성 후 저장
    3. menuItems 출력
    4. 입력값 받고, 상황별로 내용 출력(Lv 1 내용에서 배열 → 컬렉션으로 수정)

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<MenuItem> menuItems = new ArrayList<>();
 
-        String[] menu = new String[]{
-                "0. 종료      | 종료",
-                "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거",
-                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거",
-                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거",
-                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거"
-        };
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         System.out.println("[ SHAKESHACK MENU ]");
-        for (String menuItem : menu) {
-            System.out.println(menuItem);
+        for (int i = 1; i <= menuItems.size(); i++) {
+            System.out.println(i + ". " + menuItems.get(i - 1));
         }
+        System.out.println("0. 종료");
 
         boolean run = true;
         int order;
@@ -30,20 +31,9 @@ public class Main {
                         System.out.println("프로그램을 종료합니다.");
                         run = false;
                     }
-                    case 1, 2, 3, 4 -> System.out.println("주문 내용: " + menu[order]);
-                    default -> System.out.println("잘못된 번호입니다. 1부터 " + (menu.length - 1) +"까지의 번호를 입력해주세요.");
+                    case 1, 2, 3, 4 -> System.out.println("주문 내용: " + menuItems.get(order-1));
+                    default -> System.out.println("잘못된 번호입니다. 1부터 4까지의 번호를 입력해주세요.");
                 }
-
-//                if (order == 0) {
-//                    System.out.println("프로그램을 종료합니다.");
-//                    break;
-//                } else if (order >= 1 && order <= (menu.length - 1)) {
-//                    System.out.println("주문 내용: " + menu[order]);
-//                    System.out.println("주문이 완료되었습니다.");
-//                    break;
-//                } else {
-//                    System.out.println("잘못된 번호입니다. 1부터 " + (menu.length - 1) +"까지의 번호를 입력해주세요.");
-//                }
             } catch (InputMismatchException e) {
                 System.out.println("숫자만 입력해주세요.");
                 scanner.next();
