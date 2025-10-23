b29c086e07a1c23625c1238b59149e11c68f28ee
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-23 12:10:00 +0900

    Lv 4: main() 수정
    1. Menu 객체 생성 및 MenuItem 전달
    2. start() 메서드 수정사항 반영

--- src/lv4/Main.java
+++ src/lv4/Main.java
@@ -9,8 +9,10 @@ public class Main {
         MenuItem Cheeseburger = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         MenuItem Hamburger = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
 
-        Kiosk kiosk = new Kiosk(List.of(ShackBurger, SmokeShack, Cheeseburger, Hamburger));
+        Menu burgers = new Menu("Burgers");
+        burgers.saveMenus(List.of(ShackBurger, SmokeShack, Cheeseburger, Hamburger));
 
-        kiosk.start();
+        Kiosk kiosk = new Kiosk();
+        kiosk.start(burgers);
     }
 }
