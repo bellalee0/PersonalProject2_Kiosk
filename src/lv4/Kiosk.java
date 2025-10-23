22e6878bfdf506f6349bcb2806be68cc8dd3557d
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-23 12:08:59 +0900

    Lv 4: start() 수정
    1. 매개변수로 Menu 받기
    2. Menu 클래스의 printMenu() 메서드 실행

--- src/lv4/Kiosk.java
+++ src/lv4/Kiosk.java
@@ -1,28 +1,16 @@
 package lv4;
 
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
-    // 속성
-    List<MenuItem> menuItems = new ArrayList<>();
-
-    // 생성자
-    Kiosk(List<MenuItem> menus) {
-        this.menuItems = new ArrayList<>(menus);
-    }
-
-    // 기능
     // start() : 스캐너로 주문 번호 받기 → 입력된 내용에 따라 결과 출력
-    public void start() {
+    public void start(Menu menu) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("[ SHAKESHACK MENU ]");
-        for (int i = 1; i <= menuItems.size(); i++) {
-            System.out.println(i + ". " + menuItems.get(i - 1));
-        }
+        menu.printMenus();
         System.out.println("0. 종료");
 
         int order;
@@ -33,10 +21,10 @@ public class Kiosk {
                 if (order == 0) {
                     System.out.println("프로그램을 종료합니다.");
                     break;
-                } else if (order >= 1 && order <= menuItems.size()) {
-                    System.out.println("주문 내용: " + menuItems.get(order-1));
+                } else if (order >= 1 && order <= menu.menuItems.size()) {
+                    System.out.println("주문 내용: " + menu.menuItems.get(order-1));
                 } else {
-                    System.out.println("잘못된 번호입니다. 1부터 " + menuItems.size() + "까지의 번호를 입력해주세요.");
+                    System.out.println("잘못된 번호입니다. 1부터 " + menu.menuItems.size() + "까지의 번호를 입력해주세요.");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("숫자만 입력해주세요.");
