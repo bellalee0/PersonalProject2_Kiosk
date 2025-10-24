8684e31b4b8030e9c1f5e4660c6b0bffaeed9f9a
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-24 11:14:30 +0900

    Lv 5 MenuItem 클래스 필드 캡슐화 및 생성자 수정
    - 생성자 price 자료형 : double → String 변경
    - 생성자 name, price 공백 입력 시 예외 발생
    - toString() 메서드 삭제
    - getMenuItem(): 기존에 toString()에서 사용했던 내용

--- src/lv5/MenuItem.java
+++ src/lv5/MenuItem.java
@@ -2,19 +2,21 @@ package lv5;
 
 public class MenuItem {
     // 속성
-    String name;
-    double price;
-    String description;
+    private String name;
+    private double price;
+    private String description;
 
     // 생성자
-    MenuItem(String name, double price, String description) {
+    MenuItem(String name, String price, String description) {
+        if (name.trim().isEmpty() || price.trim().isEmpty()) throw new IllegalArgumentException("메뉴명과 가격은 필수입니다.");
         this.name = name;
-        this.price = price;
+        this.price = Double.parseDouble(price);
         this.description = description;
     }
 
     // 기능
-    public String toString() {
+    // getter : 메뉴명, 가격, 설명글 출력
+    String getMenuItem() {
         return name + " | W " + price + " | " + description;
     }
 }
