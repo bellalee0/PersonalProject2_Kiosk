166907c209f59e81f9e8b29e4cf4a04a3e6f694d
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-24 11:16:58 +0900

    Lv 5: 코드 리팩토링

--- src/lv5/Kiosk.java
+++ src/lv5/Kiosk.java
@@ -6,6 +6,34 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
+    // start() : 스캐너로 주문 번호 받기 → 입력된 내용에 따라 결과 출력
+    public void start(List<Menu> menuCategories) {
+        List<Menu> categories = new ArrayList<Menu>(menuCategories);
+
+        while (true) {
+            printCategories(categories);
+            int selectedCategoryNumber = getNumber("카테고리", categories);
+            if (selectedCategoryNumber == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (selectedCategoryNumber >= 1 || selectedCategoryNumber <= categories.size()) {
+                printMenuItems(categories.get(selectedCategoryNumber - 1));
+                int order;
+                boolean ordering = true;
+                while (ordering) {
+                    int selectedOrderNumber = getNumber("메뉴", categories.get(selectedCategoryNumber - 1).menuItems);
+                    if (selectedOrderNumber == 0) {
+                        System.out.println("MAIN MENU로 돌아갑니다.");
+                        ordering = false;
+                    } else if (selectedOrderNumber >= 1 || selectedOrderNumber <= categories.get(selectedCategoryNumber - 1).menuItems.size()) {
+                        System.out.println("주문 내용: " + categories.get(selectedCategoryNumber - 1).menuItems.get(selectedOrderNumber-1));
+                        printMenuItems(categories.get(selectedCategoryNumber - 1));
+                    }
+                }
+            }
+        }
+    }
+
     // printCategories(List<Menu>) : Menu 객체들의 카테고리 이름 출력
     private void printCategories(List<Menu> categories) {
         System.out.println("[ MAIN MENU ]");
@@ -34,34 +62,6 @@ public class Kiosk {
             } catch (InputMismatchException e) {
                 System.out.println("1부터 " + nameOfList.size() + "까지의 번호를 입력해주세요.");
                 scanner.next();
-            }
-        }
-    }
-
-    // start() : 스캐너로 주문 번호 받기 → 입력된 내용에 따라 결과 출력
-    public void start(List<Menu> menuCategories) {
-        List<Menu> categories = new ArrayList<Menu>(menuCategories);
-
-        while (true) {
-            printCategories(categories);
-            int selectedCategoryNumber = getNumber("카테고리", categories);
-            if (selectedCategoryNumber == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            } else if (selectedCategoryNumber >= 1 || selectedCategoryNumber <= categories.size()) {
-                printMenuItems(categories.get(selectedCategoryNumber - 1));
-                int order;
-                boolean ordering = true;
-                while (ordering) {
-                    int selectedOrderNumber = getNumber("메뉴", categories.get(selectedCategoryNumber - 1).menuItems);
-                    if (selectedOrderNumber == 0) {
-                        System.out.println("MAIN MENU로 돌아갑니다.");
-                        ordering = false;
-                    } else if (selectedOrderNumber >= 1 || selectedOrderNumber <= categories.get(selectedCategoryNumber - 1).menuItems.size()) {
-                        System.out.println("주문 내용: " + categories.get(selectedCategoryNumber - 1).menuItems.get(selectedOrderNumber-1));
-                        printMenuItems(categories.get(selectedCategoryNumber - 1));
-                    }
-                }
             }
         }
     }
