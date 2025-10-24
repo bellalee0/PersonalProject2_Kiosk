8ec5bd264dc2e4a5a5437f4bcc5f25cccc1f3ebf
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-24 10:11:24 +0900

    Lv 5 파일 생성

---
+++ src/lv5/Kiosk.java
@@ -0,0 +1,68 @@
+package lv5;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    // printCategories(List<Menu>) : Menu 객체들의 카테고리 이름 출력
+    private void printCategories(List<Menu> categories) {
+        System.out.println("[ MAIN MENU ]");
+        for (int i = 1; i <= categories.size(); i++) {
+            System.out.println(i + ". " + categories.get(i - 1));
+        }
+        System.out.println("0. 종료");
+    }
+
+    // printMenuItems(Menu) : Menu 객체로 받은 MenuItem 출력
+    private void printMenuItems(Menu menu) {
+        System.out.println("[ " + menu + " MENU ]");
+        menu.printMenus();
+        System.out.println("0. 뒤로 가기");
+    }
+
+    // getNumber(String, List<?>) : 숫자를 입력하고, 범위 외 숫자 혹은 다른 자료형 예외 처리
+    private int getNumber(String name, List<?> nameOfList) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(name + " 번호를 입력하세요: ");
+            try {
+                int selectedNumber = scanner.nextInt();
+                if (selectedNumber < 0 || selectedNumber > nameOfList.size()) { System.out.println("1부터 " + nameOfList.size() + "까지의 번호를 입력해주세요."); }
+                else return selectedNumber;
+            } catch (InputMismatchException e) {
+                System.out.println("1부터 " + nameOfList.size() + "까지의 번호를 입력해주세요.");
+                scanner.next();
+            }
+        }
+    }
+
+    // start() : 스캐너로 주문 번호 받기 → 입력된 내용에 따라 결과 출력
+    public void start(List<Menu> menuCategories) {
+        List<Menu> categories = new ArrayList<Menu>(menuCategories);
+
+        while (true) {
+            printCategories(categories);
+            int selectedCategoryNumber = getNumber("카테고리", categories);
+            if (selectedCategoryNumber == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (selectedCategoryNumber >= 1 || selectedCategoryNumber <= categories.size()) {
+                printMenuItems(categories.get(selectedCategoryNumber - 1));
+                int order;
+                boolean ordering = true;
+                while (ordering) {
+                    int selectedOrderNumber = getNumber("메뉴", categories.get(selectedCategoryNumber - 1).menuItems);
+                    if (selectedOrderNumber == 0) {
+                        System.out.println("MAIN MENU로 돌아갑니다.");
+                        ordering = false;
+                    } else if (selectedOrderNumber >= 1 || selectedOrderNumber <= categories.get(selectedCategoryNumber - 1).menuItems.size()) {
+                        System.out.println("주문 내용: " + categories.get(selectedCategoryNumber - 1).menuItems.get(selectedOrderNumber-1));
+                        printMenuItems(categories.get(selectedCategoryNumber - 1));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/lv5/Menu.java
@@ -0,0 +1,30 @@
+package lv5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    // 속성
+    List<MenuItem> menuItems;
+    String categoryName;
+
+    // 생성자(오버라이드)
+    Menu(String categoryName) {
+        this.categoryName = categoryName;
+        this.menuItems = new ArrayList<>();
+    }
+
+    // toString 오버라이드
+    @Override
+    public String toString() { return categoryName.toUpperCase(); }
+
+    // MenuItem 객체를 리스트에 저작
+    void saveMenus(List<MenuItem> menus) { this.menuItems = new ArrayList<>(menus); }
+
+    // 리스트에 있는 MenuItem을 순차적으로 프린트
+    void printMenus() {
+        for (int i = 1; i <= menuItems.size(); i++) {
+            System.out.println(i + ". " + menuItems.get(i - 1));
+        }
+    }
+}
