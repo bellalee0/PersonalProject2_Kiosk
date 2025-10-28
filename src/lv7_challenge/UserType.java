18f86c47933a96e6c2587298b6acf2a449c02266
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-28 16:34:03 +0900

    Lv 7: UserType enum 수정
    - apply() : 추상 메서드 구현 후 각 상수에 맞게 오버라이드

--- src/lv7_challenge/UserType.java
+++ src/lv7_challenge/UserType.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 
 public enum UserType {
     VETERAN(1) {
-        BigDecimal apply(BigDecimal totalPrice) {
+        public BigDecimal apply(BigDecimal totalPrice) {
             return totalPrice.multiply(new BigDecimal("0.9"));
         };
     },
     SOLDIER(2) {
-        BigDecimal apply(BigDecimal totalPrice) {
+        public BigDecimal apply(BigDecimal totalPrice) {
             return totalPrice.multiply(new BigDecimal("0.95"));
         };
     },
     STUDENT(3) {
-        BigDecimal apply(BigDecimal totalPrice) {
+        public BigDecimal apply(BigDecimal totalPrice) {
             return totalPrice.multiply(new BigDecimal("0.97"));
         };
     },
     GENERAL(4) {
-        BigDecimal apply(BigDecimal totalPrice) {
+        public BigDecimal apply(BigDecimal totalPrice) {
             return totalPrice;
         };
     };
@@ -37,4 +37,6 @@ public enum UserType {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("범위 내 숫자를 지원해주세요."));
     }
+
+    public abstract BigDecimal apply(BigDecimal totalPrice);
 }
