d50e7b2d8274dac868026425618cf60ed07c53fd
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-28 16:00:44 +0900

    Lv 7: UserType enum 구현
    - 상수 데이터 : VETERAN(국가유공자), SOLDIER(군인), STUDENT(학생), GENERAL(일반)
    - findUserType(int) : int 입력 시 해당하는 상수 반환
    - apply() : 각 상수에 맞는 할인율 적용해서 최종 금액 반환

--- src/lv7_challenge/UserType.java
+++ src/lv7_challenge/UserType.java
@@ -1,4 +1,40 @@
 package lv7_challenge;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public enum UserType {
+    VETERAN(1) {
+        BigDecimal apply(BigDecimal totalPrice) {
+            return totalPrice.multiply(new BigDecimal("0.9"));
+        };
+    },
+    SOLDIER(2) {
+        BigDecimal apply(BigDecimal totalPrice) {
+            return totalPrice.multiply(new BigDecimal("0.95"));
+        };
+    },
+    STUDENT(3) {
+        BigDecimal apply(BigDecimal totalPrice) {
+            return totalPrice.multiply(new BigDecimal("0.97"));
+        };
+    },
+    GENERAL(4) {
+        BigDecimal apply(BigDecimal totalPrice) {
+            return totalPrice;
+        };
+    };
+
+    int number;
+
+    UserType(int number) {
+        this.number = number;
+    }
+
+    public static UserType findUserType(int number) {
+        return Arrays.stream(UserType.values())
+                .filter(n -> n.number == number)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("범위 내 숫자를 지원해주세요."));
+    }
 }
